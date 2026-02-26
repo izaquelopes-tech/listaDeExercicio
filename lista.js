4d913dab4805223dc971ef7767ed1b7f9941d9ec
izaquelopes-tech <izaque.lopes@escola.pr.gov.br>
2026-02-26 14:44:39 +0000

    resolucao

---
+++ lista.js
@@ -0,0 +1,44 @@
+//criar um programa para exibir a mensagem: "Olá, mundo"
+let mensagem = 'Olá mundo';
+console.log(mensagem);
+//criar um programa para exibir seu nome completo
+let nome = 'izaque pereira lopes';
+console.log(nome);
+//criar um programa que mostre em número e exiba seu dobro
+let numero = 5;
+let dobro = numero * 2;
+console.log(dobro);
+//criar um programa que mostre em número e exiba seu triplo
+let triplo = numero * 3;
+console.log(triplo);
+//criar um programa que mostre duas notas e calcule a media 
+let nota1 = 7;
+let nota2 = 8;
+let media = (nota1 + nota2) / 2;
+console.log(media);
+// criar um programa para converter metros e centimetros 
+let metros = 1.5;
+let centimetros = metros * 100;
+console.log(centimetros);
+//criar um programa para mostrar o antecessor e sucessor de um
+let numero2 = 10;
+let antecessor = numero2 - 1;
+let sucessor = numero2 + 1;
+console.log('Antecessor: ' + antecessor);
+console.log('Sucessor: ' + sucessor);
+//criar um programa que mostre a área de um retângulo
+let base = 5;
+let altura = 3;
+let area = base * altura;
+console.log(area);
+// criar um programa para calcular o aumento de 10% num salario de 2000,00
+let salario = 2000;
+let aumento = salario * 0.10;
+let novoSalario = salario + aumento;
+console.log(novoSalario);
+//criar um programa para verificar se seu número e par ou impar
+let numero3 = 7;
+if (numero3 % 2 === 0) {
+    console.log('O número é par');
+} else {
+    console.log('O número é ímpar');
