a0e664b92c4ae640f70de5bfc3bb87085b944122
izaquelopes-tech <izaque.lopes@escola.pr.gov.br>
2026-02-27 12:34:57 +0000

    lista 2

---
+++ lista02.js
@@ -0,0 +1,9 @@
+// criar um programa  que mostre se positivo ou negativo
+
+let num1 = 99
+if (num1>=0){
+    console.log (`o numero ` $ {num1} e positivo)
+}   else {
+    console.log(`o numero ` $ {num1} e negativo)
+}
+
